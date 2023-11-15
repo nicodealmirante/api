f0e6c17c8fe4d41b2a3489d19e93c2ad23a0bf48
nicodealmirante <nicolas.e.luna@gmail.com>
2023-11-14 21:25:31 -0300

    dfsdfs

--- app.js
+++ app.js
@@ -2,16 +2,28 @@ const express = require("express");
 const { join } = require("path");
 const { createReadStream } = require("fs");
 const {
-  createProvider
+  createBot,
+  createProvider,
+  createFlow,
+  addKeyword,
 } = require("@bot-whatsapp/bot");
 
 const BaileysProvider = require("@bot-whatsapp/provider/baileys");
+const MockAdapter = require("@bot-whatsapp/database/mock");
 
-  const adapterProvider2 = createProvider(BaileysProvider);
+const flowPrincipal = addKeyword("hi").addAnswer("Hello!");
 
 const app = express();
-  const adapterProvider2 = createProvider(BaileysProvider);
+const main = async () => {
+  const adapterDB = new MockAdapter();
+  const adapterFlow = createFlow([flowPrincipal]);
+  const adapterProvider = createProvider(BaileysProvider);
 
+  createBot({
+    flow: adapterFlow,
+    provider: adapterProvider,
+    database: adapterDB,
+  });
 
   /**
    * Enviar mensaje con metodos propios del provider del bot
@@ -21,6 +33,61 @@ app.get('/enviar-mensaje', async (req, res) => {
 
 
 
-          await adapterProvider.sendText("5491159132301@s.whatsapp.net", "mensaje");
+          await adapterProvider.sendText("5491159132301@c.us", "mensaje");
       
 });
+  /**
+   * Enviar mensajes con metodos nativos del provider
+   */
+  app.get(`/sm?:${asd}`, async (req, res) => {
+    const id = "54959132301@c.us";
+    console.log(asd)
+    const templateButtons = [
+      {
+        index: 1,
+        urlButton: {
+          displayText: ":star: Star Baileys on GitHub!",
+          url: "https://github.com/adiwajshing/Baileys",
+        },
+      },
+      {
+        index: 2,
+        callButton: {
+          displayText: "Call me!",
+          phoneNumber: "+1 (234) 5678-901",
+        },
+      },
+      {
+        index: 3,
+        quickReplyButton: {
+          displayText: "This is a reply, just like normal buttons!",
+          id: "id-like-buttons-message",
+        },
+      },
+    ];
+
+    const templateMessage = {
+      text: "Hi it's a template message",
+      footer: "Hello World",
+      templateButtons: templateButtons,
+    };
+
+    const abc = await adapterProvider.getInstance();
+    await abc.sendMessage(id, templateMessage);
+
+    res.send({ data: "enviado!" });
+  });
+
+  app.get("/get-qr", async (_, res) => {
+    const YOUR_PATH_QR = join(process.cwd(), `bot.qr.png`);
+    const fileStream = createReadStream(YOUR_PATH_QR);
+
+    res.writeHead(200, { "Content-Type": "image/png" });
+    fileStream.pipe(res);
+  });
+
+  const PORT = process.env.PORT || 4000;
+  app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
+};
+
+main();
